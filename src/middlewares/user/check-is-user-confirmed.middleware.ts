7295eb9062115e3075a8c0a88589371e7378b0df
andrey-kupets <andrey.kupets@i.ua>
2022-04-19 22:09:20 +0300

    little fix in check-is-user-confirmed.middleware.ts

--- src/middlewares/user/check-is-user-confirmed.middleware.ts
+++ src/middlewares/user/check-is-user-confirmed.middleware.ts
@@ -1,14 +1,14 @@
 import { NextFunction, Response } from 'express';
 
 import { customErrors, ErrorHandler } from '../../errors';
-import { ResponseStatusCodesEnum } from '../../constants';
+import { ResponseStatusCodesEnum, UserStatusEnum } from '../../constants';
 import { IRequestExtended, IUser } from '../../models';
 
 export const checkIsUserConfirmedMiddleware =
   (req: IRequestExtended, res: Response, next: NextFunction): void => {
     const {status} = req.user as IUser;
 
-    if (!status) {
+    if (status !== UserStatusEnum.CONFIRMED) {
       return next(new ErrorHandler(
         ResponseStatusCodesEnum.FORBIDDEN,
         customErrors.FORBIDDEN_USER_NOT_CONFIRMED.message,
